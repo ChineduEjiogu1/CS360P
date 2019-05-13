317395df3516222ee639e4a270bebbae0033ecda
ChineduEjiogu1 <chineduejiogu@gmail.com>
2019-05-13 13:07:26 -0400

    redirect

--- routes/logout.js
+++ routes/logout.js
@@ -5,7 +5,8 @@ var router = express.Router();
 
 router.get('/', function(req, res){
   res.clearCookie('cookieName');
-  res.send('cookie foo cleared');
+  // res.send('cookie foo cleared');
+  res.send('<html><head><meta http-equiv="refresh" content="2; url=http://localhost:3000/" /></head> <body> <h1> BYE!! </h1></body></html>');
 });
 
 // router.get('/', function(req, res) {
